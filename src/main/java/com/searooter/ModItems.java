34dc6db39e8523d8ba14cd931bcfcb5ff2194e01
takucdn <70454164+searooter@users.noreply.github.com>
2024-10-31 00:23:44 -0400

    Moldy bryce item

--- src/main/java/com/searooter/ModItems.java
+++ src/main/java/com/searooter/ModItems.java
@@ -3,30 +3,27 @@ package com.searooter;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
 
-import java.util.IdentityHashMap;
+import java.util.function.Function;
 
 public class ModItems {
-    public static void initialize(){
+    private ModItems() {}
 
-    };
-    public static Item register(Item item, String id) {
-        // Create identifier for item  
-        Identifier itemID = Identifier.of(FirstMod.MOD_ID, id);
+    public static final Item MOLDY_BRYCE = register("moldy_bryce", Item::new, new Item.Settings());
 
-        // Register the item
-        Item registeredItem = Registry.register(Registries.ITEM, itemID, item);
-
-        return registeredItem;
+    public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("firstmod", path));
+        return Items.register(registryKey, factory, settings);
     }
 
-    public static final Item TEST_ITEM = register(
-            new Item(new Item.Settings()),
-            "test_item"
-    );
 
+    public static void initialize() {
+    }
 
 }
