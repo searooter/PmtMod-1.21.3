c224487dcba46bc9ac83cdf2791d4e49f4e9e1fe
takucdn <70454164+searooter@users.noreply.github.com>
2024-11-02 22:57:04 -0400

    Use variable instead of hard coding mod_id in item registration

--- src/main/java/com/searooter/ModItems.java
+++ src/main/java/com/searooter/ModItems.java
@@ -9,8 +9,8 @@ import net.minecraft.registry.Registry;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
-
 import java.util.function.Function;
+import static com.searooter.PmtMod.MOD_ID;
 
 public class ModItems {
     private ModItems() {}
@@ -18,7 +18,7 @@ public class ModItems {
     public static final Item MOLDY_BRYCE = register("moldy_bryce", Item::new, new Item.Settings());
 
     public static Item register(String path, Function<Item.Settings, Item> factory, Item.Settings settings) {
-        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of("pmtmod", path));
+        final RegistryKey<Item> registryKey = RegistryKey.of(RegistryKeys.ITEM, Identifier.of(MOD_ID, path));
         return Items.register(registryKey, factory, settings);
     }
 
